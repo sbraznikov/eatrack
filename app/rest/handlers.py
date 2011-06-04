3c0fc2a09be119f313d0b3ced617e2e019a95d1b
Sergej Braznikov <sergej.braznikov@symmetrics.de>
2011-06-05 01:13:09 +0200

    Added dish and eating resources

--- app/rest/handlers.py
+++ app/rest/handlers.py
@@ -1,6 +1,7 @@
+import datetime
 from piston.handler import BaseHandler
 from piston.utils import rc, throttle
-from app.rest.models import Ingredient, Dish
+from app.rest.models import Ingredient, Dish, Eating
 
 
 class IngredientHandler(BaseHandler):
@@ -105,4 +106,57 @@ class DishHandler(BaseHandler):
         except Ingredient.DoesNotExist:
             return rc.NOT_FOUND
         dish.delete()
+        return rc.DELETED
+
+
+class EatingHandler(BaseHandler):
+    allowed_methods = ('GET', 'POST', 'PUT', 'DELETE')
+    fields = ('id', 'eating_type', 'dishes',
+              'eating_at', 'comment', 'created_at',
+              'updated_at')
+    model = Eating
+
+    def read(self, request, id=None):
+        if id:
+            return Eating.objects.get(pk=id)
+        else:
+            return Eating.objects.all()
+
+    def create(self, request):
+        eating = Eating(
+            eating_type=request.POST.get('eating_type'),
+            comment=request.POST.get('comment', ''),
+            eating_at=request.POST.get('eating_at', datetime.datetime.now())
+         )
+        eating.save()
+        for key in request.POST.getlist('dishes'):
+            dish = Dish.objects.get(key=key)
+            eating.dishes.add(dish)
+        return rc.CREATED
+
+    def update(self, request, id):
+        try:
+            eating = Eating.objects.get(pk=id)
+        except Ingredient.DoesNotExist:
+            return rc.NOT_FOUND
+        if 'eating_type' in request.POST:
+            eating.eating_type = request.PUT.get('eating_type')
+        if 'eating_at' in request.POST:
+            eating.eating_at = request.PUT.get('eating_at')
+        if 'comment' in request.POST:
+            eating.comment = request.PUT.get('comment')
+        if 'dishes' in request.POST:
+            eating.dishes.clear()
+            for key in request.POST.getlist('dishes'):
+                dish = Dish.objects.get(key=key)
+                eating.dishes.add(dish)
+        eating.save()
+        return eating
+
+    def delete(self, request, id):
+        try:
+            eating = Eating.objects.get(pk=id)
+        except Ingredient.DoesNotExist:
+            return rc.NOT_FOUND
+        eating.delete()
         return rc.DELETED
--- app/rest/urls.py
+++ app/rest/urls.py
@@ -2,14 +2,17 @@ from django.conf.urls.defaults import *
 
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
-from app.rest.handlers import IngredientHandler, DishHandler
+from app.rest.handlers import IngredientHandler, DishHandler, EatingHandler
 
 ingredient_resource = Resource(handler=IngredientHandler)
 dish_resource = Resource(handler=DishHandler)
+eating_resource = Resource(handler=EatingHandler)
 
 urlpatterns = patterns('',
     (r'^ingredient/$', ingredient_resource, {'emitter_format': 'json'}),
     (r'^ingredient/(?P<id>\d+)/$', ingredient_resource, {'emitter_format': 'json'}),
     (r'^dish/$', dish_resource, {'emitter_format': 'json'}),
     (r'^dish/(?P<id>\d+)/$', dish_resource, {'emitter_format': 'json'}),
+    (r'^eating/$', eating_resource, {'emitter_format': 'json'}),
+    (r'^eating/(?P<id>\d+)/$', eating_resource, {'emitter_format': 'json'}),
 )
