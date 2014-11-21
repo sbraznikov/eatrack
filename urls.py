a2586945aba1e5be3c4d2e8d65a74b50dd895ae8
sbraznikov <sbraznikov@me.com>
2014-11-21 11:27:48 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
-    from django.contrib import admin
+from django.contrib import admin
 
-    admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-(r'^rest/', include('eatrack.app.rest.urls')),
+    (r'^rest/', include('eatrack.app.rest.urls')),
     (r'^admin/', include(admin.site.urls)),
     # (r'^media/(.*)$', 'django.views.static.serve', {'document_root': 'media', 'show_indexes': True}),
 )
